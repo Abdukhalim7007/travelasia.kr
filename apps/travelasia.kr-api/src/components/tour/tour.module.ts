3cab698eedcd4a2aa059f5152f4bcd863214a85c
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-12-26 03:18:03 +0900

    fix: import AuthModule in TourModule for RolesGuard

--- apps/travelasia.kr-api/src/components/tour/tour.module.ts
+++ apps/travelasia.kr-api/src/components/tour/tour.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TourResolver } from './tour.resolver';
 import { TourService } from './tour.service';
 import { Tour, TourSchema } from '../../schemas/Tour.model';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Tour.name, schema: TourSchema }]),
+    AuthModule,
   ],
   providers: [TourResolver, TourService],
   exports: [TourService],
 })
 export class TourModule {}
-
