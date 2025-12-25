7cac6132339e7dc7294919cd83d0b508b0b40dfc
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-12-26 00:19:12 +0900

    feat: setup graphql and add health resolver

--- apps/travelasia.kr-api/src/app.module.ts
+++ apps/travelasia.kr-api/src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true,
+      formatError: (error: any) => {
+        const graphQLFormattedError = {
+          code: error?.extensions?.code,
+          message:
+            error?.extensions?.exception?.response?.message ||
+            error?.extensions?.response?.message ||
+            error?.message,
+        };
+        console.log('GRAPHQL ERROR:', graphQLFormattedError);
+        return graphQLFormattedError;
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
 
