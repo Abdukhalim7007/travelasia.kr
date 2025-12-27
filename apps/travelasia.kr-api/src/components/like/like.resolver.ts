8befb9139c3761c31c601174eb6669fbccd540dc
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-12-27 17:10:08 +0900

    fix: register LikeTargetType enum for graphql

--- apps/travelasia.kr-api/src/components/like/like.resolver.ts
+++ apps/travelasia.kr-api/src/components/like/like.resolver.ts
@@ -24,7 +24,7 @@ export class LikeResolver {
   @Mutation(() => Boolean)
   async unlike(
     @Args('targetId') targetId: string,
-    @Args('targetType') targetType: LikeTargetType,
+    @Args('targetType', { type: () => LikeTargetType }) targetType: LikeTargetType,
     @AuthMember('_id') memberId: string,
   ): Promise<boolean> {
     return this.likeService.unlike(memberId, targetId, targetType);
@@ -34,7 +34,7 @@ export class LikeResolver {
   @Query(() => Boolean)
   async lookupAuthMemberLiked(
     @Args('targetId') targetId: string,
-    @Args('targetType') targetType: LikeTargetType,
+    @Args('targetType', { type: () => LikeTargetType }) targetType: LikeTargetType,
     @AuthMember('_id') memberId: string,
   ): Promise<boolean> {
     return this.likeService.isLiked(memberId, targetId, targetType);
--- apps/travelasia.kr-api/src/libs/enums/like.enum.ts
+++ apps/travelasia.kr-api/src/libs/enums/like.enum.ts
@@ -1,4 +1,9 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum LikeTargetType {
   TOUR = 'TOUR',
 }
 
+registerEnumType(LikeTargetType, {
+  name: 'LikeTargetType',
+});
